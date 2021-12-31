380c7b5e6b053c04ffcafb08a3407ae3f698712e
상욱 신 <shinsw6455@naver.com>
2021-12-31 10:20:36 +0900

    API 완료

--- settingweb_boot/src/main/java/com/example/settingweb_boot/service/StatisticServiceImpl.java
+++ settingweb_boot/src/main/java/com/example/settingweb_boot/service/StatisticServiceImpl.java
@@ -1,12 +1,22 @@
 package com.example.settingweb_boot.service;
 
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.settingweb_boot.dao.StatisticMapper;
+
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;;
 
 @Service
 public class StatisticServiceImpl implements StatisticService {
@@ -37,14 +47,15 @@ public class StatisticServiceImpl implements StatisticService {
 	public HashMap<String, Object> monthloginNum (String month) {
 		
 		HashMap<String, Object> retVal = new HashMap<String, Object>();
+		month = addZero(month);
 		
 		try {
 			retVal = uMapper.selectMonthLogin(month);
-			retVal.put("month", month);
+			retVal.put("month", (month.charAt(0) == '0' ? month.charAt(1) : month)); //1월~9월 사이면 앞에 0빼고 출력
 			retVal.put("is_success", true);
 		} catch(Exception e) {
 			retVal.put("totCnt", -999);
-			retVal.put("month", month);
+			retVal.put("month", (month.charAt(0) == '0' ? month.charAt(1) : month));
 			retVal.put("is_success", false);
 		}
 		
@@ -56,14 +67,15 @@ public class StatisticServiceImpl implements StatisticService {
 	public HashMap<String, Object> dayloginNum (String day) {
 		
 		HashMap<String, Object> retVal = new HashMap<String, Object>();
-		
+		day=addZero(day); //1일~9일 사이면 앞에 0을 붙임
+ 		
 		try {
 			retVal = uMapper.selectDayLogin(day);
-			retVal.put("day", day);
+			retVal.put("day", (day.charAt(0) == '0' ? day.charAt(1) : day));
 			retVal.put("is_success", true);
 		} catch(Exception e) {
 			retVal.put("totCnt", -999);
-			retVal.put("day", day);
+			retVal.put("day", (day.charAt(0) == '0' ? day.charAt(1) : day));
 			retVal.put("is_success", false);
 		}
 		
@@ -79,6 +91,7 @@ public class StatisticServiceImpl implements StatisticService {
 			retVal = uMapper.selectAverageDayLogin();
 			System.out.println("retVal : " + retVal);
 		} catch(Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 		
@@ -90,12 +103,70 @@ public class StatisticServiceImpl implements StatisticService {
 	public HashMap<String, Object> exceptHolidayloginNum(String year, String month) {
 		
 		HashMap<String, Object> retVal = new HashMap<String, Object>();
+		month = addZero(month);
+		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+		converters.add(new FormHttpMessageConverter());
+		converters.add(new StringHttpMessageConverter());
+		
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setMessageConverters(converters);
 		
 		try {
 			
-			return retVal;
+			String serviceKey = URLDecoder.decode("FbAECBbbJcpLJ4pzfUlN7K%2Bs6PF572QfIQkERi5Qbbu6ZEQC27pc453J8AIKRp0V7ejQuxMPNWJp8cG6MXaWSg%3D%3D", "UTF-8");
+			// REST API 호출
+	        String result = restTemplate.getForObject(String.format(
+	    			"http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?solYear=%s&solMonth=%s&ServiceKey=%s&_type=json"
+	    			, "20"+year, month, serviceKey), String.class);
+	        
+	        System.out.println("------------------ TEST 결과 ------------------");
+	        System.out.println(result);
+	        
+	        JSONParser parser = new JSONParser();
+	        JSONObject jsonObject = (JSONObject) parser.parse(result);
+	        JSONObject jsonObject2 = (JSONObject) jsonObject.get("response");
+	        JSONObject body = (JSONObject) jsonObject2.get("body");
+	        System.out.println(body);
+	        int num = Integer.parseInt(body.get("totalCount").toString());
+	        JSONObject items = null;
+	        if(num!=0)  items = (JSONObject) body.get("items");
+	        
+	        ArrayList<String> list = new ArrayList<>();
+	        
+	        if(num==1) {
+	        	JSONObject item = (JSONObject) items.get("item");
+	        	list.add(item.get("locdate").toString().substring(2)); // 공휴일 년, 월 더하기
+	        }
+	        else if(num!=0){
+	        	JSONArray itemList = (JSONArray) items.get("item");
+		        
+		       	for(int i=0; i<itemList.size(); i++) {
+		       		JSONObject tmp = (JSONObject)itemList.get(i);
+		       		list.add(tmp.get("locdate").toString().substring(2)); // 공휴일 년, 월 더하기
+		       	}
+	        }
+	       
+	       	for(int i=0; i<list.size(); i++) {
+	       		System.out.println(list.get(i));
+	       	}
+	       	try {
+	            retVal = uMapper.selectExceptHolidayLogin(year, month, list);
+	            retVal.put("year", year);
+	            retVal.put("month", (month.charAt(0) == '0' ? month.charAt(1) : month)); // 1월~9월 사이면 앞에 0 빼고 출력
+	            retVal.put("is_success", true);
+	        }
+	        catch(Exception e) {
+	        	e.printStackTrace();
+	            retVal.put("totCnt", -999);
+	            retVal.put("year", year);            
+	            retVal.put("month", (month.charAt(0) == '0' ? month.charAt(1) : month));
+	            retVal.put("is_success", false);
+	        }
+	            
+	        return retVal;
 			
-		} catch(Exception e) {
+		} catch(Exception e1) {
+			e1.printStackTrace();
 			return null;
 		}
 	}
@@ -106,11 +177,12 @@ public class StatisticServiceImpl implements StatisticService {
 	public HashMap<String, Object> organizationMonthloginNum (String organ, String month) {
 		
 		HashMap<String, Object> retVal = new HashMap<String, Object>();
+		month = addZero(month);
 		
 		try {
 			retVal = uMapper.selectOrganizationMonthLogin(organ, month);
-			retVal.put("organ", organ);
-			retVal.put("month", month);
+			retVal.put("organization", organ);
+			retVal.put("month", (month.charAt(0) == '0' ? month.charAt(1)  : month));
 			retVal.put("is_success", true);
 		} catch(Exception e) {
 			retVal.put("totCnt", -999);
@@ -120,5 +192,12 @@ public class StatisticServiceImpl implements StatisticService {
 		}
 		
 		return retVal;
+	}
+	
+	public String addZero(String num) {
+		if(num.length() == 1) {
+			num = "0" + num;
+		}
+		return num;
 	}
 }
